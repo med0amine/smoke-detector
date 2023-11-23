6d2c32022b96b6385ffcf6048c0cd698abe13c23
med0amine <151380756+med0amine@users.noreply.github.com>
2023-11-23 23:24:09 +0100

    Add files via upload

---
+++ smoke detector.ino
@@ -0,0 +1,38 @@
+
+
+
+int redLed = 12;
+int greenLed = 13;
+int buzzer = 2;
+int smokeA0 = A0;
+
+int sensorThres = 400;
+
+void setup() {
+  pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(smokeA0, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int analogSensor = analogRead(smokeA0);
+
+  Serial.print("Pin A0: ");
+  Serial.println(analogSensor);
+
+  if (analogSensor > sensorThres)
+  {
+    digitalWrite(redLed, HIGH);
+    digitalWrite(greenLed, LOW);
+    tone(buzzer, 1000, 200);
+  }
+  else
+  {
+    digitalWrite(redLed, LOW);
+    digitalWrite(greenLed, HIGH);
+    noTone(buzzer);
+  }
+  delay(100);
+}
